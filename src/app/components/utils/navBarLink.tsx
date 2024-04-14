3abadcf3708c8bec8c7ce24902332283b07c867d
Ibrahim Abdulrasaq <abdulrasaqalagbede@gmail.com>
2024-04-14 03:20:43 -0700

    remove /

--- src/app/components/utils/navBarLink.tsx
+++ src/app/components/utils/navBarLink.tsx
@@ -13,8 +13,14 @@ const NavBarLink = ({ text, active, url }: NavBarLinkInterface) => {
         <div className="dropdown dropdown-hover">
             <div tabIndex={0} role="button" className="m-1 text-white text-4xl">{text}</div>
             <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
-                <li><a>Item 1</Link></li>
-                <li><a>Item 2</Link></li>
+                <Link href="/">
+                    <li>Item 1</li>
+
+                </Link>
+                <Link href="/">
+                    <li>Item 2</li>
+
+                </Link>
             </ul>
         </div>
     )
