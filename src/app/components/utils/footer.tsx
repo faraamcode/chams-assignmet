393c4d8bfe8b15ca5d7f62f1b9fcf51d7572f02a
Ibrahim Abdulrasaq <abdulrasaqalagbede@gmail.com>
2024-05-14 12:57:02 +0100

    added footer

--- src/app/components/utils/footer.tsx
+++ src/app/components/utils/footer.tsx
@@ -1,54 +1,148 @@
 import Link from 'next/link'
 import React from 'react'
-import { FaFacebook, FaLinkedin, FaTwitter, FaInstagram } from 'react-icons/fa'
+import { IconType } from 'react-icons'
+import { MdLocationPin } from "react-icons/md";
+import { FaFacebook, FaLinkedin, FaTwitter, FaInstagram, FaSearchLocation, FaEnvelope, FaPhoneAlt } from 'react-icons/fa'
 
 const Footer = () => {
+    const data: IFooterArticle[] = [
+        {
+            title: "Wonder Sprout Academy",
+            discription: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus cumque amet culpa eaque. Omnis excepturi rerum perferendis vero sunt nam.",
+            icons: [
+                {
+                    icon: FaFacebook,
+                    url: "https://www.twitter.com"
+                },
+                {
+                    icon: FaLinkedin,
+                    url: "https://www.twitter.com"
+                },
+                {
+                    icon: FaInstagram,
+                    url: "https://www.twitter.com"
+                },
+                {
+                    icon: FaTwitter,
+                    url: "https://www.twitter.com"
+                },
+            ]
+        },
+        {
+            title: "Admission",
+            links: [
+                {
+                    text: "creche",
+                    url: "/"
+                },
+                {
+                    text: "nursery and primary",
+                    url: "/"
+                },
+                {
+                    text: "college",
+                    url: "/"
+                }
+            ],
+        },
+        {
+            title: "Coutact us",
+            discription: "Address:42 gaa akanbi, Ilorin, kwara state, Nigeria.",
+            icons: [
+                {
+                    icon: MdLocationPin,
+                    url: "https://www.twitter.com"
+                },
+                {
+                    icon: FaEnvelope,
+                    url: "https://www.twitter.com"
+                },
+                {
+                    icon: FaPhoneAlt,
+                    url: "https://www.twitter.com"
+                },
+            ]
+        },
+        {
+            title: "School portal",
+            links: [
+                {
+                    text: "primary school",
+                    url: "/"
+                },
+                {
+                    text: "college",
+                    url: "/"
+                },
+                {
+                    text: "staff",
+                    url: "/"
+                },
+            ],
+        },
+    ]
     const year = new Date().getFullYear()
     return (
-        <footer className="footer">
-            {/* <!-- social icons --> */}
-            {/* <ul className="nav-links">
-                <li>
-                    <Link href="/">home</Link>
-                </li>
-                <li>
-                    <Link href="/">about us</Link>
-                </li>
-                <li>
-                    <Link href="/">Admission</Link>
-                </li>
-                <li>
-                    <Link href="/">contact us</Link>
-                </li>
-            </ul> */}
-            <ul className="social-icons">
-                <li>
-                    <Link href="https://www.twitter.com" className="social-icon">
-                        <FaFacebook />
-                    </Link>
-                </li>
-                <li>
-                    <Link href="https://www.twitter.com" className="social-icon">
-                        <FaTwitter />
-                    </Link>
-                </li>
-                <li>
-                    <Link href="https://www.twitter.com" className="social-icon">
-                        <FaInstagram />
-                    </Link>
-                </li>
-                <li>
-                    <Link href="https://www.twitter.com" className="social-icon">
-                        <FaLinkedin />
-                    </Link>
-                </li>
-            </ul>
-            <div className='courtesy'>
+        <footer className="section footer-new">
+            <div className="section-center base-section">
+                {
+                    data.map((item, index) =>
+                        <FooterArticle key={index} {...item} />
+                    )
+                }
+            </div>
+            <div className='footer-coutersy'>
+                <div className='divider'></div>
                 <p>&copy; <span id="date">{year}</span> wonder sprout academy. all rights reserved</p>
 
             </div>
         </footer>
 
+    )
+}
+
+interface ILink {
+    text: string;
+    url: string
+}
+interface ICons {
+    url: string;
+    icon: IconType
+}
+interface IFooterArticle {
+    title?: string
+    discription?: string;
+    links?: ILink[];
+    icons?: ICons[];
+}
+const FooterArticle = ({ title, discription, links, icons }: IFooterArticle) => {
+    return (
+        <article className='footer-article'>
+            {title && <h5>{title}</h5>}
+            {discription && <p>{discription}</p>}
+            {links && <ul className="footer-links">
+                {
+                    links.map(
+                        (item) =>
+                            <li key={item.text}>
+                                <Link href={item.url}>{item.text}</Link>
+                            </li>
+                    )
+                }
+            </ul>}
+            {icons && <ul className="footer-social-icons">
+                {
+                    icons.map((item) =>
+                        <li key={item.url}>
+                            <Link href={item.url} className="footer-social-icon">
+                                {<item.icon />}
+                            </Link>
+                        </li>
+                    )
+                }
+            </ul>}
+
+        </article>
     )
 }
 
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Wonder Sprout Acadamy",
   description: "Citadel of learning",
+  icons: {
+    icon: "https://asset.cloudinary.com/faraamit/b4a68768d9520649b0c9b555702eac5a"
+  }
 };
 
 export default function RootLayout({
@@ -16,6 +19,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+      </head>
       <body className={inter.className} suppressHydrationWarning={true}>{children}</body>
     </html>
   );
